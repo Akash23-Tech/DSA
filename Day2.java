c4e26c3bde89bc59ff0a8a502b6b5dd1181d8abe
Akash23-Tech <kulalakash788@gmail.com>
2026-01-12 17:52:36 +0530

    find max and min element

--- Day2.java
+++ Day2.java
@@ -1,6 +1,25 @@
 //Find the maximum and minimum element in an array
 
-public class MaxMinArray {
+class MaxMinArray {
+    public static void main(String[] args) {
+        
+        int[] arr = {12,15,3,8,6};
 
+        int max = arr[0];
+        int min = arr[0];
+
+        for ( int i = 1; i<arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+            if (arr[i] < min) {
+                min = arr[i];
+                
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+        System.out.println("Minimum element: " + min);
+    }
     
 }
