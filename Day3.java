fb7e2372a91dce12a47e4028ab8af7d40c5e1814
Akash23-Tech <kulalakash788@gmail.com>
2026-01-13 17:41:27 +0530

    Finish the code

--- Day3.java
+++ Day3.java
@@ -1,6 +1,19 @@
 //Find the "Kth" max and min element of an array
 
+import java.util.Arrays;
  class KthSmallest {
-    public static int kthSmallest(int)
+    public static int kthSmallest(int[] arr, int k) {
+
+        Arrays.sort(arr);
+        return arr [k-1];
+    }
+
+    public static void main(String[] args) {
+        
+        int [] arr = {5, 8, 12, 3, 23, 45, 2, 9};
+        int k = 3;
+
+        System.out.println(kthSmallest(arr, k));
+    }
     
 }
