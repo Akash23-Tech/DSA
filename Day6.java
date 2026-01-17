1ffc39b07b5da22fd9ae380a8c0ea6dda3d437b8
Akash23-Tech <kulalakash788@gmail.com>
2026-01-17 18:41:19 +0530

    DAy 6 problem solved union and intersaction

--- Day6.java
+++ Day6.java
@@ -1,18 +1,30 @@
+// Find the Union and Intersection of the two sorted arrays.
 
 import java.util.HashSet;
 
  class UnionIntersection {
     public static void main(String[] args) {
         
-        int a[] = {1,3,5,4};
+        int a[] = {1,3,5,4,6};
         int b[] = {5,3,6,2};
 
         HashSet<Integer> set = new HashSet<>();
 
+        HashSet<Integer> result = new HashSet<>();
+
         for(int i=0; i<a.length; i++){
             set.add(a[i]);
         }
-        System.out.println(set);
+
+        for(int i=0; i<b.length; i++){
+            if(set.contains(b[i])){
+                result.add(b[i]);
+            }
+        }
+         System.out.println(set);
+        System.out.println(result);
+
+        
 
     }
 }
