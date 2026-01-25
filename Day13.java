066bd5153111a49d18ec21688cd792ada16a9469
Akash23-Tech <kulalakash788@gmail.com>
2026-01-25 18:40:40 +0530

    Merge Overlapping Intervals Problem completed

--- Day13.java
+++ Day13.java
@@ -7,24 +7,24 @@ class Solution {
         if (arr == null || arr.length == 0)
             return new int[0][0];
 
-        // Step 1: Sort intervals by start time
+        
         Arrays.sort(arr, (a, b) -> a[0] - b[0]);
 
         List<int[]> result = new ArrayList<>();
 
-        // Step 2: Add first interval
+        
         int[] current = arr[0];
         result.add(current);
 
-        // Step 3: Merge overlapping intervals
+        
         for (int i = 1; i < arr.length; i++) {
             int[] next = arr[i];
 
             if (next[0] <= current[1]) {
-                // Overlapping case
+                
                 current[1] = Math.max(current[1], next[1]);
             } else {
-                // Non-overlapping case
+            
                 current = next;
                 result.add(current);
             }
