23bbaf3b57206d5619a1120fe48cf9d7c413fa03
Akash23-Tech <kulalakash788@gmail.com>
2026-01-29 18:37:02 +0530

    Day17 problem Find all pairs on integer array whose sum is equal to given number edit main function

--- Day17.java
+++ Day17.java
@@ -1,3 +1,29 @@
-public class Day17 {
-    
+// Find all pairs on integer array whose sum is equal to given number
+
+import java.util.HashSet;
+import java.util.Set;
+
+class PairSum {
+
+    public static void findPairs(int[] arr, int target) {
+        Set<Integer> seen = new HashSet<>();
+
+        for (int num : arr) {
+            int complement = target - num;
+
+            if (seen.contains(complement)) {
+                System.out.println(complement + ", " + num);
+            }
+
+            seen.add(num);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 3, 5, 6, -2, 4, 7};
+        int target = 7;
+
+        findPairs(arr, target);
+    }
 }
+
