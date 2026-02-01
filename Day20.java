e995716ec93448dc741b6efef3d8f8be45897e43
Akash23-Tech <kulalakash788@gmail.com>
2026-02-01 17:13:21 +0530

    Day20 problem Find if there is any subarray with sum equal to 0 problem completed

--- Day20.java
+++ Day20.java
@@ -1,3 +1,46 @@
-public class Day20 {
-    
+// Find if there is any subarray with sum equal to 0
+
+import java.util.HashMap;
+
+class ZeroSumSubarray {
+
+    static void printZeroSumSubarray(int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int prefixSum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            prefixSum += arr[i];
+
+            // Case 1: Subarray from 0 to i
+            if (prefixSum == 0) {
+                System.out.print("Zero-sum subarray: ");
+                printArray(arr, 0, i);
+                return;
+            }
+
+            // Case 2: Subarray from map.get(prefixSum)+1 to i
+            if (map.containsKey(prefixSum)) {
+                int start = map.get(prefixSum) + 1;
+                System.out.print("Zero-sum subarray: ");
+                printArray(arr, start, i);
+                return;
+            }
+
+            map.put(prefixSum, i);
+        }
+
+        System.out.println("No zero-sum subarray found");
+    }
+
+    static void printArray(int[] arr, int start, int end) {
+        for (int i = start; i <= end; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4, 2, -3, 1, 6};
+        printZeroSumSubarray(arr);
+    }
 }
