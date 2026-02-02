2ab0202f368f50faf6ac3004d6114559809fc422
Akash23-Tech <kulalakash788@gmail.com>
2026-02-02 17:15:12 +0530

    Day21 problem Find the Factorial of a large number problem completed

--- Day21.java
+++ Day21.java
@@ -1,3 +1,29 @@
-public class Day21 {
-    
+// Find the Factorial of a large number
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+class LargeFactorial{
+
+    static BigInteger factorial(int n) {
+        BigInteger result = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = Sc.nextInt();
+
+        System.out.println("Factorial of " + n + " is:");
+        System.out.println(factorial(n));
+
+        Sc.close();
+    }
 }
