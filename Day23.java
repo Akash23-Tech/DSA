958ed8d643b219fcec768bc2a6cf798aad39accb
Akash23-Tech <kulalakash788@gmail.com>
2026-02-06 16:24:59 +0530

    Day23 problem Changes in output

--- Day23.java
+++ Day23.java
@@ -35,6 +35,7 @@ class LongestConsecutiveSubsequence {
     public static void main(String[] args) {
         int[] arr = {100, 5, 200, 1, 3, 2, 4};
         System.out.println(longestConsecutive(arr)); 
+        System.out.println("Day23 problem completed");
     }
 }
 
