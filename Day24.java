631a717aee1bbaaaedfa920fd977b3791ff8927a
Akash23-Tech <kulalakash788@gmail.com>
2026-02-07 17:21:58 +0530

    Day24 Given an array of size n and a number k, fin all elements that appear more than n/k times problem completed

--- Day24.java
+++ Day24.java
@@ -1,3 +1,30 @@
-public class Day24 {
+import java.util.*;
+
+class MoreThanNByK {
+
+    public static void findElements(int[] arr, int k) {
+        int n = arr.length;
+
+        // Step 1: Count frequency of each element
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // Step 2: Print elements appearing more than n/k times
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n / k) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
     
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 2, 2, 1, 2, 3, 3};
+        int k = 4;
+
+        findElements(arr, k);
+    }
 }
