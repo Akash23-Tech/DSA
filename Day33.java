95311bc4fb873f49f4ef387d69c550468201f7ed
Akash23-Tech <kulalakash788@gmail.com>
2026-02-20 18:25:15 +0530

    Day33 problem Minimum no. of operations required to make an array palindrome solved

--- Day33.java
+++ Day33.java
@@ -1,5 +1,28 @@
 // Minimum no. of operations required to make an array palindrome
 
-public class Day33 {
-    
+class Solution{
+    static boolean isPalindrome(int n) {
+        String s = Integer.toString(n);
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - 1 - i))
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean isPalinArray(int[] arr) {
+      
+        for (int i = 0; i < arr.length; i++) {
+            if (! isPalindrome(arr[i]))
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 121, 131, 20 };
+        boolean res = isPalinArray(arr);
+        System.out.println(res);
+    }
 }
