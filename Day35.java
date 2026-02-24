b3e0b38ce0263f847985c32626aab2e2086eea13
Akash23-Tech <kulalakash788@gmail.com>
2026-02-24 18:34:16 +0530

    Day35 problem Median of 2 sorted arrays of different size problem solved

--- Day35.java
+++ Day35.java
@@ -1,5 +1,47 @@
 // Median of 2 sorted arrays of different size
 
-public class Day35 {
-    
+class MedianAfterMerge {
+
+    public static double findMedian(int[] a, int[] b) {
+        int n = a.length;
+        int m = b.length;
+
+        int[] merged = new int[n + m];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < n && j < m) {
+            if (a[i] <= b[j]) {
+                merged[k++] = a[i++];
+            } else {
+                merged[k++] = b[j++];
+            }
+        }
+
+        while (i < n) {
+            merged[k++] = a[i++];
+        }
+
+        while (j < m) {
+            merged[k++] = b[j++];
+        }
+
+        int total = n + m;
+
+        if (total % 2 == 1) {
+            return merged[total / 2];
+        }
+        // If even
+        else {
+            return (merged[(total / 2) - 1] + merged[total / 2]) / 2.0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 5, 6, 12, 15};
+        int[] b = {3, 4, 6, 10, 10, 12};
+
+        double median = findMedian(a, b);
+        System.out.println("Median: " + median);
+    }
 }
