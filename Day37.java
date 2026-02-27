9a6f0e1f14a45bb093cb964d0a6d9bb8bcb24db1
Akash23-Tech <kulalakash788@gmail.com>
2026-02-27 20:49:26 +0530

    Day37 problem Search an element in a Matrix problem solved

--- Day37.java
+++ Day37.java
@@ -1,5 +1,36 @@
 // Search an element in a Matrix
 
-public class Day37 {
-    
+class SearchInSortedMatrix {
+
+    public static boolean search(int[][] mat, int x) {
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int row = 0;
+        int col = m - 1; // start from top-right
+
+        while (row < n && col >= 0) {
+            if (mat[row][col] == x) {
+                return true;
+            } else if (mat[row][col] > x) {
+                col--;        // move left
+            } else {
+                row++;        // move down
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {   
+        int[][] mat = {
+            {3, 30, 38},
+            {20, 52, 54},
+            {35, 60, 69}
+        };
+
+        int x = 62;
+
+        boolean result = search(mat, x);
+        System.out.println(result);
+    }
 }
