13b0ba378df7fa8aef44622d478122cbffbbbc19
Akash23-Tech <kulalakash788@gmail.com>
2026-03-11 21:26:25 +0530

    Day43 DSA Problem Rotate an Image 90 Degree Clockwise problem solved

---
+++ Day43.java
@@ -0,0 +1,47 @@
+// Rotate an Image 90 Degree Clockwise
+
+class RotateMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int n = mat.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = mat[i][left];
+                mat[i][left] = mat[i][right];
+                mat[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        // Print rotated matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
