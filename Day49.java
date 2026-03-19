ef9dcf921f025a755319c48c5888f45c28c7e966
Akash23-Tech <kulalakash788@gmail.com>
2026-03-19 19:29:29 +0530

    Day49 DSA Problem Write a Code to check whether one string is a rotation of another problem solved

--- Day49.java
+++ Day49.java
@@ -1,5 +1,25 @@
 // Write a Code to check whether one string is a rotation of another
 
-public class Day49 {
-    
+class StringRotation {
+    public static boolean isRotation(String s1, String s2) {
+
+        if (s1.length() != s2.length() || s1.length() == 0) {
+            return false;
+        }
+
+        String temp = s1 + s1;
+
+        return temp.contains(s2);
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcd";
+        String s2 = "cdab";
+
+        if (isRotation(s1, s2)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
 }
