1676f0c77d46b22586ee4b9ba9227359236e602f
Akash23-Tech <kulalakash788@gmail.com>
2026-03-24 17:36:40 +0530

    Day51 Dsa problem Count and Say problem has solved

---
+++ Day51.java
@@ -0,0 +1,36 @@
+// Count and Say problem
+
+class LookAndSay{
+
+    public static String nthRow(int n) {
+        if (n <= 0) return "";
+
+        String result = "1"; // first row
+
+        for (int i = 2; i <= n; i++) {
+            StringBuilder temp = new StringBuilder();
+            int count = 1;
+
+            for (int j = 1; j < result.length(); j++) {
+                if (result.charAt(j) == result.charAt(j - 1)) {
+                    count++;
+                } else {
+                    temp.append(count).append(result.charAt(j - 1));
+                    count = 1;
+                }
+            }
+
+            // append last group
+            temp.append(count).append(result.charAt(result.length() - 1));
+
+            result = temp.toString();
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(nthRow(n));
+    }
+}
