b4d861be6469746f90ed99d794699fc3ee485f15
Akash23-Tech <kulalakash788@gmail.com>
2026-03-29 18:57:01 +0530

    Day55 Dsa problem Print all the permutations of the given string problem solved

--- Day55.java
+++ Day55.java
@@ -1,5 +1,27 @@
 // Print all the permutations of the given string
 
-public class Day55 {
-    
+class Day55 {
+    public static void main(String[] args) {
+        String s = "ABC"; // change input here
+        permute(s, "");
+    }
+
+    static void permute(String str, String ans) {
+        // Base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        // Pick each character one by one
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            // Remaining string after removing chosen character
+            String remaining = str.substring(0, i) + str.substring(i + 1);
+
+            // Recursive call
+            permute(remaining, ans + ch);
+        }
+    }
 }
