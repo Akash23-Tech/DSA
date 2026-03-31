7bd0ce1f1daa049a38c00e9cc4cbd494c9a91868
Akash23-Tech <kulalakash788@gmail.com>
2026-03-31 18:38:56 +0530

    Day56 Dsa problem solved changes in output problem solved

--- Day56.java
+++ Day56.java
@@ -1,3 +1,38 @@
-public class Day56 {
-    
+// Split the binary string into substrings with equal number of 0s and 1s
+
+class BalancedBinaryString{
+
+    public static int maxBalancedSubstrings(String s) {
+        int count0 = 0;
+        int count1 = 0;
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '0') {
+                count0++;
+            } else if (ch == '1') {
+                count1++;
+            }
+
+            // When counts are equal → one balanced substring found
+            if (count0 == count1) {
+                result++;
+            }
+        }
+
+        // If total 0s and 1s are not equal → not possible
+        if (count0 != count1) {
+            return -1;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "0100110101";
+        int ans = maxBalancedSubstrings(s);
+        System.out.println(ans);  
+    }
 }
