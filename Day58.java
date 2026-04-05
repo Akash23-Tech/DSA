aee8386f5f855dfc0c58852480f5a6bcb4fd2c16
Akash23-Tech <kulalakash788@gmail.com>
2026-04-05 19:55:32 +0530

    Day58 Dsa problem completed

--- Day58.java
+++ Day58.java
@@ -1,5 +1,45 @@
 // EDIT Distance [Very Imp]
 
-public class Day58 {
-    
+class EditDistance {
+
+    public static int minOperations(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Base cases
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i; // delete all characters
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j; // insert all characters
+        }
+
+        // Fill DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1]; // no operation
+                } else {
+                    int insert = dp[i][j - 1];
+                    int delete = dp[i - 1][j];
+                    int replace = dp[i - 1][j - 1];
+
+                    dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "Akash";
+        String s2 = "Akas";
+
+        int result = minOperations(s1, s2);
+        System.out.println("Minimum operations: " + result);
+    }
 }
