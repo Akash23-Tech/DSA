2296a62501d60d6dd066f63e87eb8818573675ac
Akash23-Tech <kulalakash788@gmail.com>
2026-04-22 17:33:07 +0530

    Day65 Dsa problem Minimum number of bracket reversals needed to make an expression balanced problem solved

--- Day65.java
+++ Day65.java
@@ -1,5 +1,43 @@
 // Minimum number of bracket reversals needed to make an expression balanced.
 
-public class Day65 {
-    
+import java.util.Stack;
+
+class Solution {
+    static int minReversals(String s) {
+        int n = s.length();
+
+        // If odd length, cannot be balanced
+        if (n % 2 != 0) return -1;
+
+        Stack<Character> stack = new Stack<>();
+
+        // Remove balanced pairs
+        for (char ch : s.toCharArray()) {
+            if (ch == '{') {
+                stack.push(ch);
+            } else {
+                if (!stack.isEmpty() && stack.peek() == '{') {
+                    stack.pop(); // balanced pair
+                } else {
+                    stack.push(ch);
+                }
+            }
+        }
+
+        // Count remaining unbalanced brackets
+        int open = 0, close = 0;
+
+        while (!stack.isEmpty()) {
+            if (stack.pop() == '{') open++;
+            else close++;
+        }
+
+        // Calculate reversals
+        return (open + 1) / 2 + (close + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+        String s = "}{{}}{{{";
+        System.out.println(minReversals(s));
+    }
 }
