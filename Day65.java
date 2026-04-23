12455c7193b523d10a561a9edf947374da90d852
Akash23-Tech <kulalakash788@gmail.com>
2026-04-23 19:30:21 +0530

    Day65 Dsa problem changes in output

--- Day65.java
+++ Day65.java
@@ -6,12 +6,10 @@ class Solution {
     static int minReversals(String s) {
         int n = s.length();
 
-        // If odd length, cannot be balanced
         if (n % 2 != 0) return -1;
 
         Stack<Character> stack = new Stack<>();
 
-        // Remove balanced pairs
         for (char ch : s.toCharArray()) {
             if (ch == '{') {
                 stack.push(ch);
@@ -23,21 +21,18 @@ class Solution {
                 }
             }
         }
-
-        // Count remaining unbalanced brackets
         int open = 0, close = 0;
 
         while (!stack.isEmpty()) {
             if (stack.pop() == '{') open++;
             else close++;
         }
-
-        // Calculate reversals
         return (open + 1) / 2 + (close + 1) / 2;
     }
 
     public static void main(String[] args) {
         String s = "}{{}}{{{";
         System.out.println(minReversals(s));
+        System.out.println("Day65 Dsa problem solved");
     }
 }
