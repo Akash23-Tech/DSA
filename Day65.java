378151e14b973e03ea1fe5dc05dced7b43ec68f8
Akash23-Tech <kulalakash788@gmail.com>
2026-04-24 17:05:41 +0530

    Day65 Dsa problem changes in input values

--- Day65.java
+++ Day65.java
@@ -31,7 +31,7 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        String s = "}{{}}{{{";
+        String s = "}{{}}{{{}{";
         System.out.println(minReversals(s));
         System.out.println("Day65 Dsa problem solved");
     }
