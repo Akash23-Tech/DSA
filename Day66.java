86e7bc46153885aa7fe9b1dab8b163c5e8bd9d2b
Akash23-Tech <kulalakash788@gmail.com>
2026-04-25 18:59:03 +0530

    Day65 Dsa problem Count All Palindromic Subsequence in a given String problem solved

--- Day66.java
+++ Day66.java
@@ -1,5 +1,43 @@
 // Count All Palindromic Subsequence in a given String.
 
-public class Day66 {
-    
+class Main {
+
+    static int[][] memo;
+
+    static int countPalindrome(String str, int i, int j) {
+        // Base cases
+        if (i > j) return 0;
+        if (i == j) return 1;
+
+        if (memo[i][j] != -1) {
+            return memo[i][j];
+        }
+
+        if (str.charAt(i) == str.charAt(j)) {
+            memo[i][j] = countPalindrome(str, i + 1, j)
+                       + countPalindrome(str, i, j - 1)
+                       + 1;
+        } else {
+            memo[i][j] = countPalindrome(str, i + 1, j)
+                       + countPalindrome(str, i, j - 1)
+                       - countPalindrome(str, i + 1, j - 1);
+        }
+
+        return memo[i][j];
+    }
+
+    public static void main(String[] args) {
+        String str = "aaa";
+        int n = str.length();
+
+        memo = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        System.out.println(countPalindrome(str, 0, n - 1));
+    }
 }
