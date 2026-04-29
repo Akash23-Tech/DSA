7b074aa2bbe9348a9e48fe46d1438eccd67285e6
Akash23-Tech <kulalakash788@gmail.com>
2026-04-29 17:29:00 +0530

    Day67 Dsa problem Count of number of given string in 2D character array problem solved

--- Day67.java
+++ Day67.java
@@ -1,5 +1,65 @@
 // Count of number of given string in 2D character array
 
-public class Day67 {
-    
+class WordSearch2D{
+
+    static int R = 4, C = 5;
+
+    static int[] dx = {-1, 1, 0, 0}; // up, down, left, right
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) {
+
+        char[][] mat = {
+                {'S','N','B','S','N'},
+                {'B','A','K','E','A'},
+                {'B','K','B','B','K'},
+                {'S','E','B','S','E'}
+        };
+
+        String target = "SNAKES";
+
+        int result = countOccurrences(mat, target);
+        System.out.println("Output: " + result);
+    }
+
+    static int countOccurrences(char[][] mat, String word) {
+        int count = 0;
+        boolean[][] visited = new boolean[R][C];
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                if (mat[i][j] == word.charAt(0)) {
+                    count += dfs(mat, word, i, j, 0, visited);
+                }
+            }
+        }
+        return count;
+    }
+
+    static int dfs(char[][] mat, String word, int x, int y, int index, boolean[][] visited) {
+
+        // If full word matched
+        if (index == word.length()) {
+            return 1;
+        }
+
+        if (x < 0 || y < 0 || x >= R || y >= C ||
+            visited[x][y] || mat[x][y] != word.charAt(index)) {
+            return 0;
+        }
+
+        // Mark visited
+        visited[x][y] = true;
+
+        int count = 0;
+
+        for (int dir = 0; dir < 4; dir++) {
+            count += dfs(mat, word, x + dx[dir], y + dy[dir], index + 1, visited);
+        }
+
+        // Backtrack
+        visited[x][y] = false;
+
+        return count;
+    }
 }
